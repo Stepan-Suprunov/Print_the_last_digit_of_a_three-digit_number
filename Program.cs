1e25ad3a16db1affd61fce85c844f87c9795bfa6
Stepan_Suprunov <ipsuprunovso@mail.ru>
2022-06-09 20:09:43 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿//Вывести последнее число из трёхзначного
+Console.WriteLine("Введите трёхзначное число");
+int Number = int.Parse(Console.ReadLine());
+Number = Number % 10;
+Console.WriteLine(Number);
